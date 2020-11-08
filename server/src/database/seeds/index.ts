bb6c5b64418598432418e8d7e99fa09810cc9ebb
PHILLIP <pebd0001@student.monash.edu>
2020-11-08 14:48:13 +1100

    style: adding index files for entities for easier mass import

---
+++ server/src/database/seeds/index.ts
@@ -0,0 +1,10 @@
+import CreateStaff from "./CreateStaff";
+import CreateUnit from "./CreateUnit";
+import CreateAllocation from "./CreateAllocation";
+import CreateActivity from "./CreateActivity";
+import CreateAvailability from "./CreateAvailability";
+import CreateStaffPreference from "./CreateStaffPreference";
+
+export {
+    CreateStaff, CreateUnit, CreateActivity, CreateAllocation, CreateAvailability, CreateStaffPreference
+};
---
+++ server/src/entity/index.ts
@@ -0,0 +1,10 @@
+import { Staff } from "./Staff";
+import { Unit } from "./Unit";
+import { Allocation } from "./Allocation";
+import { Activity } from "./Activity";
+import { Availability } from "./Availability";
+import { StaffPreference } from "./StaffPreference";
+
+export {
+    Staff, Unit, Activity, Allocation, Availability, StaffPreference
+};
