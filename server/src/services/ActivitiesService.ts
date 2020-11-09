b8f9f4aab150d89de4589e25d5ceb37ad52fcb2b
Uyen Tran <utra0001@student.monash.edu>
2020-11-09 17:05:01 +1100

    fix: fix ActivitiesService POST request and path param

--- server/src/services/ActivitiesService.ts
+++ server/src/services/ActivitiesService.ts
@@ -1,14 +1,14 @@
 import { DeleteResult, getRepository } from "typeorm";
 import {
-  Server,
-  Path,
+  DELETE,
   GET,
+  Path,
   PathParam,
   POST,
-  DELETE,
-  PATCH,
+  PUT,
+  QueryParam,
 } from "typescript-rest";
-import { Allocation } from "~/entity";
+import { Unit } from "~/entity";
 import { Activity } from "../entity/Activity";
 
 @Path("/activities")
@@ -21,8 +21,14 @@ class ActivitiesService {
    */
   @GET
   public getAllActivities(): Promise<Array<Activity>> {
+    /**
+     * Note on relations option in find
+     * - Currently the relations are not eager or lazy relations (those can be more convenient but come with their own drawbacks)
+     * so we need to specifiy the relations in find* methods
+     * - To also load nested relation, i.e relations: ["allocations", "allocations.staff"]
+     */
     return this.repo.find({
-      relations: ["allocations", "unit"]
+      relations: ["allocations", "unit"], // TODO: think about which relations should be fetch to avoid performance issue
     });
   }
 
@@ -32,14 +38,14 @@ class ActivitiesService {
    * @return Activity single activity
    */
   // TODO: assert return value as Promise<Activity> here
+  // TODO: changed activityCode to activityId since activityCode is not unique/primary key
   @GET
-  @Path(":activityCode")
-  public async getActivity(@PathParam("activityCode") activityCode: string) {
+  @Path(":activityId")
+  public async getActivity(@PathParam("activityId") id: string) {
     let activity = await this.repo.findOne(
-      { activityCode: activityCode },
+      { id },
       { relations: ["allocations", "unit"] }
     );
-
     return activity;
   }
 
@@ -49,7 +55,14 @@ class ActivitiesService {
    * @return Activity new activity
    */
   @POST
-  public createActivity(newRecord: Activity): Promise<Activity> {
+  public async createActivity(
+    newRecord: Activity,
+    @QueryParam("unitId") unitId: string
+  ): Promise<Activity> {
+    // TODO: better optimisation since it's wasteful to fetch unit when we don't use it
+    // potential solution: https://github.com/typeorm/typeorm/issues/447
+    let unit = await getRepository(Unit).findOneOrFail({ id: unitId });
+    newRecord.unit = unit;
     return this.repo.save(this.repo.create(newRecord));
   }
 
@@ -58,7 +71,7 @@ class ActivitiesService {
    * @param changedActivity new activity object to change existing activity to
    * @return Activity changed activity
    */
-  @PATCH
+  @PUT
   public async updateActivity(changedActivity: Activity): Promise<Activity> {
     let activityToUpdate = await this.repo.findOne(
       changedActivity.activityCode
@@ -73,10 +86,10 @@ class ActivitiesService {
    * @return DeleteResult result of delete request
    */
   @DELETE
-  @Path(":activityCode")
+  @Path(":activityId")
   public deleteActivity(
-    @PathParam("activityCode") activityCode: string
+    @PathParam("activityId") id: string
   ): Promise<DeleteResult> {
-    return this.repo.delete({ activityCode: activityCode });
+    return this.repo.delete({ id });
   }
 }
