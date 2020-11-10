ca3920bac776203e5dcd8f934ef94960f5df30b0
Cheong Chee Feng <cche0106@student.monash.edu>
2020-11-10 20:40:51 +0800

    style: format with prettier

--- client/src/pages/Staff.tsx
+++ client/src/pages/Staff.tsx
@@ -1,28 +1,25 @@
-import React, {Fragment, useState, useEffect} from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
+import React, {useState, useEffect } from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 const Staff = () => {
+  const [staffs, setStaffs] = useState<IStaff[]>([]);
 
-  const[staffs, setStaffs] = useState<IStaff[]>([]);
-
-  const getAllStaff = async() =>{
-    try{
+  const getAllStaff = async () => {
+    try {
       const response = await fetch("http://localhost:8888/staff");
       const jsonData = await response.json();
-      
+
       setStaffs(jsonData);
-    } catch(err){
+    } catch (err) {
       console.error(err.message);
     }
-  }
+  };
 
   useEffect(() => {
     getAllStaff();
@@ -32,39 +29,45 @@ const Staff = () => {
     <div id="main">
       <h1>Staff</h1>
       <TableContainer component={Paper}>
-      <Table className={""} size="small" aria-label="a dense table" >
-        <TableHead>
-          <TableRow>
-            <TableCell>Staff ID</TableCell>
-            <TableCell align="right">Last Name</TableCell>
-            <TableCell align="right">First Name</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">AQF</TableCell>
-            <TableCell align="right">Studying AQF</TableCell>
-            <TableCell align="center">Preference</TableCell>
-            <TableCell align="center">Availability</TableCell>
-            <TableCell align="center">Allocation</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {staffs.map(staff => (
-            <TableRow key={staff.id}>
-              <TableCell component="th" scope="row">
-                {staff.id}
-              </TableCell>
-              <TableCell align="right">{staff.lastName}</TableCell>
-              <TableCell align="right">{staff.givenNames}</TableCell>
-              <TableCell align="right">{staff.email}</TableCell>
-              <TableCell align="right">{staff.aqf}</TableCell>
-              <TableCell align="right">{staff.studyingAqf}</TableCell>
-              <TableCell align="justify"><Button variant="contained" color="default">View Preferences</Button></TableCell>
-              <TableCell align="right"><Button variant="contained" color="primary">View Availability</Button></TableCell>
-              <TableCell align="right"><Button variant="contained" color="secondary">View Allocations</Button></TableCell>
+        <Table className={""} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Staff ID</TableCell>
+              <TableCell align="right">Last Name</TableCell>
+              <TableCell align="right">First Name</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">AQF</TableCell>
+              <TableCell align="right">Studying AQF</TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {staffs.map((staff) => (
+              <TableRow key={staff.id}>
+                <TableCell component="th" scope="row">
+                  {staff.id}
+                </TableCell>
+                <TableCell align="right">{staff.lastName}</TableCell>
+                <TableCell align="right">{staff.givenNames}</TableCell>
+                <TableCell align="right">{staff.email}</TableCell>
+                <TableCell align="right">{staff.aqf}</TableCell>
+                <TableCell align="right">{staff.studyingAqf}</TableCell>
+                <TableCell align="right">
+                  <button>View Preferences</button>
+                </TableCell>
+                <TableCell align="right">
+                  <button>View Availability</button>
+                </TableCell>
+                <TableCell align="right">
+                  <button>View Allocations</button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
