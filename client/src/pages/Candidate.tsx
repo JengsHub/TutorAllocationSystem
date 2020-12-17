9c97c77ad71f8b5887026bb8926e78ad0b7daff8
Cheong Chee Feng <cche0106@student.monash.edu>
2020-12-17 15:10:05 +0800

    feat: lecture making offer

--- client/src/pages/Candidate.tsx
+++ client/src/pages/Candidate.tsx
@@ -7,16 +7,21 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
+import DatabaseFinder from "../apis/DatabaseFinder";
 
 const Candidate = () => {
   let location = useLocation();
 
-  // TODO: this activityId can use a different way to obtain, such as useparams but it is undefined for some reason if possible someone can solve it
+  // TODO: this activityId can use a different way to obtain, such as useparams
+  // but it is undefined for some reason if possible someone can solve it
   let activityId = location.pathname.substring(12);
   const [candidatesPreference, setCandidatePreference] = useState<
     IPreferences[]
   >([]);
   const [activity, setActivity] = useState<IActivity>();
+  const [selecteds, setSelected] = useState<number[]>([]);
 
   const getCandidatePreference = async () => {
     const res = await fetch(
@@ -25,7 +30,8 @@ const Candidate = () => {
     return res.json();
   };
 
-  // TODO: instead of calling api again, actually can pass the activity object selected to this component, but i also dk how to do it.
+  // TODO: instead of calling api again, actually can pass the activity object
+  // selected to this component, but i also dk how to do it.
   const getActivity = async () => {
     const res = await fetch("http://localhost:8888/activities/" + activityId);
     return res.json();
@@ -36,12 +42,63 @@ const Candidate = () => {
       // console.log(res);
       setActivity(res);
     });
-    // console.log(activityId);
+    console.log(activityId);
     getCandidatePreference().then((res) => {
       // console.log(res);
       setCandidatePreference(res);
     });
-  });
+  }, []);
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelecteds = candidatesPreference.map((_, i) => i);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, i: number) => {
+    const selectedIndex = selecteds.indexOf(i);
+    let newSelected: number[] = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selecteds, i);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selecteds.slice(1));
+    } else if (selectedIndex === selecteds.length - 1) {
+      newSelected = newSelected.concat(selecteds.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selecteds.slice(0, selectedIndex),
+        selecteds.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const makeOffers = () => {
+    console.log(selecteds);
+    selecteds.forEach(async (i) => {
+      console.log(candidatesPreference[i]);
+      var allocation: Allocation = {
+        activityId: activityId,
+        staffId: candidatesPreference[i].staffId,
+      };
+      try {
+        await DatabaseFinder.post("/allocations", allocation);
+      } catch (err) {
+        throw err;
+      }
+    });
+    setSelected([]);
+    
+    getCandidatePreference().then((res) => {
+      // console.log(res);
+      setCandidatePreference(res);
+    });
+  };
+  const isSelected = (i: number) => selecteds.indexOf(i) !== -1;
 
   return (
     <div id="main">
@@ -57,13 +114,24 @@ const Candidate = () => {
             {activity.startTime}-{activity.endTime} {activity.dayOfWeek}
           </h5>
         </div>
-      ) : (
-        console.log(false)
-      )}
+      ) : null}
       <TableContainer component={Paper}>
         <Table className={""} size="small" aria-label="a dense table">
           <TableHead>
             <TableRow>
+              <TableCell>
+                <Checkbox
+                  indeterminate={
+                    selecteds.length > 0 &&
+                    selecteds.length < candidatesPreference.length
+                  }
+                  checked={
+                    selecteds.length > 0 &&
+                    selecteds.length == candidatesPreference.length
+                  }
+                  onChange={handleSelectAllClick}
+                />
+              </TableCell>
               <TableCell>Staff First Name</TableCell>
               <TableCell align="right">Staff Last Name</TableCell>
               <TableCell align="right">Preference Score</TableCell>
@@ -73,7 +141,10 @@ const Candidate = () => {
           </TableHead>
           <TableBody>
             {candidatesPreference.map((candidatePreference, i) => (
-              <TableRow key={i}>
+              <TableRow key={i} onClick={(event) => handleClick(event, i)}>
+                <TableCell>
+                  <Checkbox checked={isSelected(i)}/>
+                </TableCell>
                 <TableCell>{candidatePreference.staff.givenNames}</TableCell>
                 <TableCell align="right">
                   {candidatePreference.staff.lastName}
@@ -92,6 +163,7 @@ const Candidate = () => {
           </TableBody>
         </Table>
       </TableContainer>
+      <Button onClick={makeOffers}> Request Offer</Button>
     </div>
   );
 };
