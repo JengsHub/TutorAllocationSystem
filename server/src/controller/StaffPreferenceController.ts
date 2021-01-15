8e88b77ae98e26e77fe2b88fd16e7400ac654e4d
aseem-thakar <41178612+aseem-thakar@users.noreply.github.com>
2021-01-15 13:02:44 +1100

    feat/api: role-based controller for staff prefs

--- server/src/controller/StaffPreferenceController.ts
+++ server/src/controller/StaffPreferenceController.ts
@@ -1,4 +1,7 @@
+import { Staff, Unit } from "~/entity";
+import { StaffPreference } from "~/entity/StaffPreference";
 import { AppRoleEnum, RoleEnum } from "~/enums/RoleEnum";
+import { UnauthorisedAccessedError } from "~/helpers/shortcuts";
 
 export class StaffPreferenceControllerFactory {
   getController(role: RoleEnum | AppRoleEnum): IStaffPreferenceController {
@@ -15,10 +18,204 @@ export class StaffPreferenceControllerFactory {
   }
 }
 
-export interface IStaffPreferenceController {}
+export interface IStaffPreferenceController {
+  getAllStaffPreferences(): any;
+  getStaffPreference(id: string): any;
+  createStaffPreference(
+    newRecord: StaffPreference,
+    staff: Staff,
+    unit: Unit,
+    me: Staff
+  ): any;
+  updateStaffPreference(
+    changedStaffPreference: StaffPreference,
+    me: Staff
+  ): any;
+  deleteStaffPreference(id: string, me: Staff): any;
+}
 
-class TaStaffPreferenceController implements IStaffPreferenceController {}
+class TaStaffPreferenceController implements IStaffPreferenceController {
+  getAllStaffPreferences() {
+    return new UnauthorisedAccessedError("TA cannot get all staff preferences");
+  }
 
-class LecturerStaffPreferenceController implements IStaffPreferenceController {}
+  getStaffPreference(id: string) {
+    return StaffPreference.findOne({ id }, { relations: ["staff", "unit"] });
+  }
 
-class AdminStaffPreferenceController implements IStaffPreferenceController {}
+  async createStaffPreference(
+    newRecord: StaffPreference,
+    staff: Staff,
+    unit: Unit,
+    me: Staff
+  ) {
+    if (me == staff) {
+      let staffPreferenceToUpdate = await StaffPreference.findOne({
+        staff: staff,
+        unit: unit,
+      });
+
+      if (staffPreferenceToUpdate) {
+        StaffPreference.update({ id: staffPreferenceToUpdate.id }, newRecord);
+        newRecord.staff = staff;
+        newRecord.unit = unit;
+        return newRecord;
+      }
+
+      newRecord.staff = staff;
+      newRecord.unit = unit;
+
+      return StaffPreference.save(StaffPreference.create(newRecord));
+    } else {
+      return new UnauthorisedAccessedError(
+        "Can't create preference for staff other than yourself"
+      );
+    }
+  }
+
+  async updateStaffPreference(
+    changedStaffPreference: StaffPreference,
+    me: Staff
+  ) {
+    if (me == changedStaffPreference.staff) {
+      let staffPreferenceToUpdate = await StaffPreference.findOne({
+        id: changedStaffPreference.id,
+      });
+      staffPreferenceToUpdate = changedStaffPreference;
+      return StaffPreference.save(staffPreferenceToUpdate);
+    } else {
+      return new UnauthorisedAccessedError(
+        "Can't update preference for staff other than yourself"
+      );
+    }
+  }
+
+  async deleteStaffPreference(id: string, me: Staff) {
+    let preference = await StaffPreference.findOneOrFail({
+      id: id,
+    });
+    if (me == preference.staff) {
+      return StaffPreference.delete({ id });
+    } else {
+      return new UnauthorisedAccessedError(
+        "Can't delete preference for staff other than yourself"
+      );
+    }
+  }
+}
+
+class LecturerStaffPreferenceController implements IStaffPreferenceController {
+  getAllStaffPreferences() {
+    return new UnauthorisedAccessedError(
+      "Lecturers cannot get all staff preferences"
+    );
+  }
+
+  getStaffPreference(id: string) {
+    return StaffPreference.findOne({ id }, { relations: ["staff", "unit"] });
+  }
+
+  async createStaffPreference(
+    newRecord: StaffPreference,
+    staff: Staff,
+    unit: Unit,
+    me: Staff
+  ) {
+    if (me == staff) {
+      let staffPreferenceToUpdate = await StaffPreference.findOne({
+        staff: staff,
+        unit: unit,
+      });
+
+      if (staffPreferenceToUpdate) {
+        StaffPreference.update({ id: staffPreferenceToUpdate.id }, newRecord);
+        newRecord.staff = staff;
+        newRecord.unit = unit;
+        return newRecord;
+      }
+
+      newRecord.staff = staff;
+      newRecord.unit = unit;
+
+      return StaffPreference.save(StaffPreference.create(newRecord));
+    } else {
+      return new UnauthorisedAccessedError(
+        "Can't create preference for staff other than yourself"
+      );
+    }
+  }
+
+  async updateStaffPreference(
+    changedStaffPreference: StaffPreference,
+    me: Staff
+  ) {
+    if (me == changedStaffPreference.staff) {
+      let staffPreferenceToUpdate = await StaffPreference.findOne({
+        id: changedStaffPreference.id,
+      });
+      staffPreferenceToUpdate = changedStaffPreference;
+      return StaffPreference.save(staffPreferenceToUpdate);
+    } else {
+      return new UnauthorisedAccessedError(
+        "Can't update preference for staff other than yourself"
+      );
+    }
+  }
+
+  async deleteStaffPreference(id: string, me: Staff) {
+    let preference = await StaffPreference.findOneOrFail({
+      id: id,
+    });
+    if (me == preference.staff) {
+      return StaffPreference.delete({ id });
+    } else {
+      return new UnauthorisedAccessedError(
+        "Can't delete preference for staff other than yourself"
+      );
+    }
+  }
+}
+
+class AdminStaffPreferenceController implements IStaffPreferenceController {
+  getAllStaffPreferences() {
+    return StaffPreference.find({ relations: ["staff", "unit"] });
+  }
+
+  getStaffPreference(id: string) {
+    return StaffPreference.findOne({ id }, { relations: ["staff", "unit"] });
+  }
+
+  async createStaffPreference(
+    newRecord: StaffPreference,
+    staff: Staff,
+    unit: Unit
+  ) {
+    let staffPreferenceToUpdate = await StaffPreference.findOne({
+      staff: staff,
+      unit: unit,
+    });
+    if (staffPreferenceToUpdate) {
+      StaffPreference.update({ id: staffPreferenceToUpdate.id }, newRecord);
+      newRecord.staff = staff;
+      newRecord.unit = unit;
+      return newRecord;
+    }
+
+    newRecord.staff = staff;
+    newRecord.unit = unit;
+
+    return StaffPreference.save(StaffPreference.create(newRecord));
+  }
+
+  async updateStaffPreference(changedStaffPreference: StaffPreference) {
+    let staffPreferenceToUpdate = await StaffPreference.findOne({
+      id: changedStaffPreference.id,
+    });
+    staffPreferenceToUpdate = changedStaffPreference;
+    return StaffPreference.save(staffPreferenceToUpdate);
+  }
+
+  deleteStaffPreference(id: string) {
+    return StaffPreference.delete({ id });
+  }
+}
