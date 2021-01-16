f60d1208180d20fd74fffbb0d198086b44381bd8
Jason Toh <Weilun Jason, Toh>
2021-01-17 01:36:17 +0800

    feat(Dashboard): rearrange buttons and status

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -9,153 +9,247 @@ const Dashboard = () => {
   return (
     <div id="main">
       <h1>Dashboard</h1>
-      <CustomButton
-        id="red-btn"
-        value=""
-        type="button"
-        isCross
-        isRed
-        onButtonClick={handleButtonClick}
-      />
-      <CustomButton
-        id="green-btn"
-        value=""
-        type="button"
-        isCheck
-        isGreen
-        onButtonClick={handleButtonClick}
-      />
-      <CustomButton
-        id="trash-btn"
-        value=""
-        type="button"
-        isRed
-        isTrash
-        onButtonClick={handleButtonClick}
-      />
-      <CustomButton
-        id="swap-btn"
-        value="Swap"
-        type="button"
-        isBlue
-        onButtonClick={handleButtonClick}
-      />
-      <CustomButton
-        id="cancel-swap-btn"
-        value="Cancel Swap"
-        type="button"
-        isRed
-        onButtonClick={handleButtonClick}
-      />
-      <CustomButton
-        id="add-btn"
-        value="Add"
-        type="button"
-        isGrey
-        onButtonClick={handleButtonClick}
-      />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <CustomButton
+            id="red-btn"
+            value=""
+            type="button"
+            isCross
+            isRed
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="green-btn"
+            value=""
+            type="button"
+            isCheck
+            isGreen
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="trash-btn"
+            value=""
+            type="button"
+            isRed
+            isTrash
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="swap-btn"
+            value="Swap"
+            type="button"
+            isBlue
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="cancel-swap-btn"
+            value="Cancel Swap"
+            type="button"
+            isRed
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="add-btn"
+            value="Add"
+            type="button"
+            isGrey
+            onButtonClick={handleButtonClick}
+          />
+        </div>
 
-      <p></p>
-      <CustomStatus
-        id="waiting-res-btn"
-        value="Waiting for response"
-        isBlue
-        isClock
-      />
-      <CustomStatus
-        id="waiting-res-btn"
-        value="Error With Approval"
-        isRed
-        isExclamationTriangle
-      />
-      <CustomStatus
-        id="waiting-res-btn"
-        value="Swap Offered"
-        isGreen
-        isExclamationCircle
-      />
-      <CustomStatus id="yellow-btn" value="Accepted Offer" isBlue isCheck />
-      <CustomStatus id="yellow-btn" value="Confirmed" isGreen isCheck />
-      <CustomStatus
-        id="yellow-btn"
-        value="Swap Available"
-        isYellow
-        isExclamationDiamond
-      />
-      <CustomStatus id="yellow-btn" value="Accepted Offer" isGreen isCheck />
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <CustomButton
+            id="red-btn"
+            value=""
+            type="button"
+            isCross
+            isRed
+            isSmall
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="green-btn"
+            value=""
+            type="button"
+            isCheck
+            isGreen
+            isSmall
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="trash-btn"
+            value=""
+            type="button"
+            isRed
+            isTrash
+            isSmall
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="swap-btn"
+            value="Swap"
+            type="button"
+            isBlue
+            isSmall
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="cancel-swap-btn"
+            value="Cancel Swap"
+            type="button"
+            isRed
+            isSmall
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="add-btn"
+            value="Add"
+            type="button"
+            isGrey
+            isSmall
+            onButtonClick={handleButtonClick}
+          />
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <CustomButton
+            id="red-btn"
+            value=""
+            type="button"
+            isCross
+            isRed
+            isCompact
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="green-btn"
+            value=""
+            type="button"
+            isCheck
+            isGreen
+            isCompact
+            onButtonClick={handleButtonClick}
+          />
+          <CustomButton
+            id="trash-btn"
+            value=""
+            type="button"
+            isRed
+            isTrash
+            isCompact
+            onButtonClick={handleButtonClick}
+          />
+        </div>
+      </div>
 
-      <CustomStatus
-        id="yellow-btn"
-        value="TA has rejected"
-        isRed
-        isExclamationTriangle
-      />
-      <CustomStatus
-        id="yellow-btn"
-        value="Workforce has approved"
-        isGreen
-        isCheck
-      />
-      <CustomStatus
-        id="yellow-btn"
-        value="Workforce has rejected"
-        isRed
-        isCross
-      />
-      <CustomStatus
-        id="yellow-btn"
-        value="Lecturer has approved"
-        isGreen
-        isCheck
-      />
-      <CustomStatus id="yellow-btn" value="No actions yet" isGrey />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <CustomStatus
+            id="waiting-res-btn"
+            value="Waiting for response"
+            isBlue
+            isClock
+          />
+          <CustomStatus
+            id="waiting-res-btn"
+            value="Error With Approval"
+            isRed
+            isExclamationTriangle
+          />
+          <CustomStatus
+            id="waiting-res-btn"
+            value="Swap Offered"
+            isGreen
+            isExclamationCircle
+          />
+          <CustomStatus id="yellow-btn" value="Accepted Offer" isBlue isCheck />
+          <CustomStatus id="yellow-btn" value="Confirmed" isGreen isCheck />
+          <CustomStatus
+            id="yellow-btn"
+            value="Swap Available"
+            isYellow
+            isExclamationDiamond
+          />
+          <CustomStatus
+            id="yellow-btn"
+            value="Accepted Offer"
+            isGreen
+            isCheck
+          />
 
-      <p></p>
-
-      <CustomStatus
-        id="waiting-res-btn"
-        value="Waiting for response"
-        isBlue
-        isClock
-        isSmall
-      />
-      <CustomStatus
-        id="waiting-res-btn"
-        value="Error With Approval"
-        isRed
-        isExclamationTriangle
-        isSmall
-      />
-      <CustomStatus
-        id="waiting-res-btn"
-        value="Swap Offered"
-        isGreen
-        isExclamationCircle
-        isSmall
-      />
-
-      <p></p>
-      <CustomStatus
-        id="waiting-res-btn"
-        value="Waiting for response"
-        isBlue
-        isClock
-        isCompact
-      />
-      <CustomStatus
-        id="waiting-res-btn"
-        value="Error With Approval"
-        isRed
-        isExclamationTriangle
-        isCompact
-      />
-      <CustomStatus
-        id="waiting-res-btn"
-        value="Swap Offered"
-        isGreen
-        isExclamationCircle
-        isCompact
-      />
+          <CustomStatus
+            id="yellow-btn"
+            value="TA has rejected"
+            isRed
+            isExclamationTriangle
+          />
+          <CustomStatus
+            id="yellow-btn"
+            value="Workforce has approved"
+            isGreen
+            isCheck
+          />
+          <CustomStatus
+            id="yellow-btn"
+            value="Workforce has rejected"
+            isRed
+            isCross
+          />
+          <CustomStatus
+            id="yellow-btn"
+            value="Lecturer has approved"
+            isGreen
+            isCheck
+          />
+          <CustomStatus id="yellow-btn" value="No actions yet" isGrey />
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <CustomStatus
+            id="waiting-res-btn"
+            value="Waiting for response"
+            isBlue
+            isClock
+            isSmall
+          />
+          <CustomStatus
+            id="waiting-res-btn"
+            value="Error With Approval"
+            isRed
+            isExclamationTriangle
+            isSmall
+          />
+          <CustomStatus
+            id="waiting-res-btn"
+            value="Swap Offered"
+            isGreen
+            isExclamationCircle
+            isSmall
+          />
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <CustomStatus
+            id="waiting-res-btn"
+            value="Waiting for response"
+            isBlue
+            isClock
+            isCompact
+          />
+          <CustomStatus
+            id="waiting-res-btn"
+            value="Error With Approval"
+            isRed
+            isExclamationTriangle
+            isCompact
+          />
+          <CustomStatus
+            id="waiting-res-btn"
+            value="Swap Offered"
+            isGreen
+            isExclamationCircle
+            isCompact
+          />
+        </div>
+      </div>
     </div>
   );
 };
