2d2c91455383f95e5fc35def3151face52f301f3
Teo Wei Han <wteo0013@student.monash.edu>
2021-01-24 12:26:17 +0800

    feat: search functionality for staffs

--- client/src/pages/Staff/Staff.tsx
+++ client/src/pages/Staff/Staff.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -6,19 +7,35 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
 import baseApi from "../../apis/baseApi";
 import StaffModal from "./StaffModal";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
+    },
+  })
+);
+
 const Staff = () => {
+  const [input, setInput] = useState<string>("");
   const [staffs, setStaffs] = useState<IStaff[]>([]);
+  const [staffDefault, setStaffsDefault] = useState<IStaff[]>([]);
   const [staffModalOpen, setStaffModalOpen] = useState<string | null>(null);
   const [staffName, setStaffName] = useState<string>("");
+  const classes = useStyles();
 
   const getAllStaff = async () => {
     try {
       const res = await baseApi.get("/staff/all");
       const jsonData = await res.data;
       setStaffs(jsonData);
+      setStaffsDefault(jsonData);
     } catch (err) {
       console.error(err.message);
     }
@@ -27,6 +44,16 @@ const Staff = () => {
   useEffect(() => {
     getAllStaff();
   }, []);
+
+  const updateInput = async (input: string) => {
+    const filtered = staffDefault.filter((staff) => {
+      return (staff.givenNames + " " + staff.lastName)
+        .toLowerCase()
+        .includes(input.toLowerCase());
+    });
+    setInput(input);
+    setStaffs(filtered);
+  };
 
   const onClickStaff = (staff: IStaff) => {
     setStaffModalOpen(staff.id);
@@ -41,6 +68,15 @@ const Staff = () => {
         closeModal={() => setStaffModalOpen(null)}
       />
       <h1>Staff</h1>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          id="searchStaffInput"
+          label="Search staff"
+          variant="outlined"
+          value={input}
+          onChange={(e) => updateInput(e.target.value)}
+        />
+      </form>
       <TableContainer component={Paper}>
         <Table className={""} size="small" aria-label="a dense table">
           <TableHead>
