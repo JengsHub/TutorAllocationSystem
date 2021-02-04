3397fe481b0f64aaf6c0dfabb5fd524b7653c6e9
Teo Wei Han <wteo0013@student.monash.edu>
2021-02-04 17:22:57 +0800

    doc: update role and rule api doc

--- server/src/openAPI/role.swagger.ts
+++ server/src/openAPI/role.swagger.ts
@@ -1,10 +1,28 @@
+import { roleDef } from "./definition/roleDef.swagger";
+import { staffDef } from "./definition/staffDef.swagger";
+import { unitDef } from "./definition/unitDef.swagger";
+
 export const role = {
   "/roles": {
     get: {
       tags: ["Role"],
+      summary: "Get all roles",
+      operationId: "getRoles",
+      produces: ["application/json"],
       responses: {
         "200": {
           description: "successful operation",
+          schema: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                ...roleDef.properties,
+                staff: staffDef,
+                unit: unitDef,
+              },
+            },
+          },
         },
       },
     },
@@ -13,9 +31,13 @@ export const role = {
   "/roles/{id}": {
     get: {
       tags: ["Role"],
+      summary: "Return a role",
+      operationId: "getRoleById",
+      produces: ["application/json"],
       responses: {
         "200": {
           description: "successful operation",
+          schema: roleDef,
         },
       },
     },
@@ -24,25 +46,85 @@ export const role = {
   "/roles/unit/{unitId}": {
     get: {
       tags: ["Role"],
+      summary: "Gets a unit's roles",
+      operationId: "getRolesByUnit",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "unitId",
+          description: "Unit Id",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: {
+            type: "array",
+            items: roleDef,
+          },
         },
       },
     },
+
     post: {
       tags: ["Role"],
+      summary: "Creates a role record for the unit",
+      operationId: "createRole",
+      consumes: ["application.json"],
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "body",
+          name: "newRecord",
+          description: "new role object",
+          required: true,
+          type: "string",
+        },
+        {
+          in: "path",
+          name: "unitId",
+          description: "Unit Id",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: roleDef,
         },
       },
     },
+
     put: {
       tags: ["Role"],
+      summary: "Updates a role record for the unit",
+      operationId: "updateRole",
+      consumes: ["application.json"],
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "body",
+          name: "changedRecord",
+          description: "updated role object",
+          required: true,
+          type: "string",
+        },
+        {
+          in: "path",
+          name: "unitId",
+          description: "Unit Id",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: roleDef,
         },
       },
     },
@@ -51,9 +133,22 @@ export const role = {
   "/roles/unit/{unitId}/me": {
     get: {
       tags: ["Role"],
+      summary: "Get role of the user for a unit",
+      operationId: "getMyRoleByUnit",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "unitId",
+          description: "Unit Id",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: roleDef,
         },
       },
     },
@@ -62,9 +157,29 @@ export const role = {
   "/roles/unit/{unitId}/{roleId}": {
     delete: {
       tags: ["Role"],
+      summary: "Deletes a role",
+      operationId: "deleteRole",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "unitId",
+          description: "Unit Id",
+          required: true,
+          type: "string",
+        },
+        {
+          in: "path",
+          name: "roleId",
+          description: "Role Id",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: roleDef,
         },
       },
     },
--- server/src/openAPI/rule.swagger.ts
+++ server/src/openAPI/rule.swagger.ts
@@ -1,19 +1,48 @@
+import { ruleDef } from "./definition/ruleDef.swagger";
+
 export const rule = {
   "/rules": {
     get: {
       tags: ["Rule"],
+      summary: "Returns a list of the global rules",
+      operationId: "getAllRules",
+      produces: ["application/json"],
       responses: {
         "200": {
           description: "successful operation",
+          schema: {
+            type: "array",
+            items: ruleDef,
+          },
         },
       },
     },
 
     put: {
       tags: ["Rule"],
+      summary: "Updates a global rule",
+      operationId: "updateRules",
+      consumes: ["application.json"],
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "body",
+          name: "changedRules",
+          description: "Updated rule objects",
+          required: true,
+          schema: {
+            type: "array",
+            items: ruleDef,
+          },
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: {
+            type: "array",
+            items: ruleDef,
+          },
         },
       },
     },
@@ -22,9 +51,22 @@ export const rule = {
   "/rules/{id}": {
     get: {
       tags: ["Rule"],
+      summary: "Returns a rule",
+      operationId: "getRule",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "id",
+          description: "ruleId",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: ruleDef,
         },
       },
     },
