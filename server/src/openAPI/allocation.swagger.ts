3a82df9bb92916b0b6f13af742c0d7c1453ec1b3
Teo Wei Han <wteo0013@student.monash.edu>
2021-02-04 15:52:00 +0800

    doc: update allocation and availability api doc

--- server/src/openAPI/allocation.swagger.ts
+++ server/src/openAPI/allocation.swagger.ts
@@ -1,29 +1,95 @@
+import { activityDef } from "./definition/activityDef.swagger";
+import { allocationDef } from "./definition/allocationDef.swagger";
+import { staffDef } from "./definition/staffDef.swagger";
+import { unitDef } from "./definition/unitDef.swagger";
+
 export const allocation = {
   "/allocations": {
     post: {
       tags: ["Allocation"],
+      summary: "Creates an allocation",
+      operationId: "createAllocation",
+      consumes: ["application.json"],
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "body",
+          name: "body",
+          description: "New allocation object",
+          required: true,
+          schema: allocationDef,
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: allocationDef,
         },
       },
     },
+
     put: {
       tags: ["Allocation"],
+      summary: "Updates an allocation",
+      operationId: "updateAllocation",
+      consumes: ["application.json"],
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "body",
+          name: "body",
+          description: "Updated allocation object",
+          required: true,
+          schema: allocationDef,
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: allocationDef,
         },
       },
     },
   },
 
+  //different return depending on is it unitId filetered
   "/allocations/mine": {
     get: {
       tags: ["Allocation"],
+      summary: "Get the allocated activities of the current user",
+      operationId: "getMyAllocation",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "query",
+          name: "unitId",
+          description: "Filter allocations with unitId",
+          required: false,
+          type: "string",
+        },
+        {
+          in: "query",
+          name: "isLecturerApproved",
+          description: "Filter allocations with lecturerApproved true or false",
+          required: false,
+          type: "boolean",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                ...allocationDef.properties,
+                staff: staffDef,
+                unit: unitDef,
+                activity: activityDef,
+              },
+            },
+          },
         },
       },
     },
@@ -32,9 +98,49 @@ export const allocation = {
   "/allocations/unswapped": {
     get: {
       tags: ["Allocation"],
+      summary: "Get unswapped allocations",
+      operationId: "getUnswappedAllocation",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "query",
+          name: "unitId",
+          description: "Filter allocations with unitId",
+          required: false,
+          type: "string",
+        },
+        {
+          in: "query",
+          name: "isLecturerApproved",
+          description:
+            "Filter allocations with isLecturerApproved true or false",
+          required: false,
+          type: "boolean",
+        },
+        {
+          in: "query",
+          name: "isWorkforceApproved",
+          description:
+            "Filter allocations with isWorkforceApproved true or false",
+          required: false,
+          type: "boolean",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                ...allocationDef.properties,
+                staff: staffDef,
+                unit: unitDef,
+                activity: activityDef,
+              },
+            },
+          },
         },
       },
     },
@@ -43,17 +149,44 @@ export const allocation = {
   "/allocations/{id}": {
     get: {
       tags: ["Allocation"],
+      summary: "Get an allocation with id",
+      operationId: "getAllocation",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "id",
+          description: "allocationID",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: allocationDef,
         },
       },
     },
+
     delete: {
       tags: ["Allocation"],
+      summary: "Delete an allocation with id",
+      operationId: "deleteAllocation",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "id",
+          description: "allocationID",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: allocationDef,
         },
       },
     },
@@ -62,9 +195,29 @@ export const allocation = {
   "/allocations/{id}/lecturer-approval": {
     patch: {
       tags: ["Allocation"],
+      summary: "Update lecturer approval status for specified allocation",
+      operationId: "updateLecturerApproval",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "id",
+          description: "allocationID",
+          required: true,
+          type: "string",
+        },
+        {
+          in: "query",
+          name: "value",
+          description: "updated value",
+          required: true,
+          type: "boolean",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: allocationDef,
         },
       },
     },
@@ -73,9 +226,29 @@ export const allocation = {
   "/allocations/{id}/workforce-approval": {
     patch: {
       tags: ["Allocation"],
+      summary: "Update acceptance status for the specified allocation",
+      operationId: "updateTaAcceptance",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "id",
+          description: "allocationID",
+          required: true,
+          type: "string",
+        },
+        {
+          in: "query",
+          name: "value",
+          description: "updated value",
+          required: true,
+          type: "boolean",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: allocationDef,
         },
       },
     },
@@ -84,9 +257,29 @@ export const allocation = {
   "/allocations/{id}/ta-acceptance": {
     patch: {
       tags: ["Allocation"],
+      summary: "Update workforce approval status for the specified allocation",
+      operationId: "updateWorkforceApproval",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "id",
+          description: "allocationID",
+          required: true,
+          type: "string",
+        },
+        {
+          in: "query",
+          name: "value",
+          description: "updated value",
+          required: true,
+          type: "boolean",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: allocationDef,
         },
       },
     },
--- server/src/openAPI/availability.swagger.ts
+++ server/src/openAPI/availability.swagger.ts
@@ -1,26 +1,74 @@
+import { availabilityDef } from "./definition/availabilityDef.swagger";
+
 export const availability = {
   "/availabilities": {
     get: {
       tags: ["Availability"],
+      summary: "Returns a list of availabilities",
+      operationId: "getAllAvailabilities",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "query",
+          name: "staffId",
+          description: "Staff Id",
+          required: false,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: {
+            type: "array",
+            items: availabilityDef,
+          },
         },
       },
     },
+
     post: {
       tags: ["Availability"],
+      summary: "Creates an availability",
+      operationId: "createAvailability",
+      consumes: ["application.json"],
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "body",
+          name: "newRecord",
+          description: "New availability object",
+          required: true,
+          schema: availabilityDef,
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: availabilityDef,
         },
       },
     },
+
     put: {
       tags: ["Availability"],
+      summary: "Updates an availability",
+      operationId: "updateAvailability",
+      consumes: ["application.json"],
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "body",
+          name: "changedAvailability",
+          description: "Updated availability object",
+          required: true,
+          schema: availabilityDef,
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: availabilityDef,
         },
       },
     },
@@ -29,17 +77,44 @@ export const availability = {
   "/availabilities/{id}": {
     get: {
       tags: ["Availability"],
+      summary: "Returns an availability",
+      operationId: "getAvailability",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "id",
+          description: "Availability Id",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: availabilityDef,
         },
       },
     },
+
     delete: {
       tags: ["Availability"],
+      summary: "Deletes an availability",
+      operationId: "deleteAvailability",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "id",
+          description: "Availability Id",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: availabilityDef,
         },
       },
     },
@@ -48,9 +123,54 @@ export const availability = {
   "/availabilities/monToFriAvai/{year}": {
     get: {
       tags: ["Availability"],
+      summary:
+        "Returns a list of availabilities lists, of staff in order of Monday to Friday",
+      operationId: "getYearAvailabilities",
+      produces: ["application/json"],
+      parameters: [
+        {
+          in: "path",
+          name: "year",
+          description: "Search availabilities with year",
+          required: true,
+          type: "string",
+        },
+        {
+          in: "query",
+          name: "staffId",
+          description: "Search availabilities of staff with id = staffId",
+          required: true,
+          type: "string",
+        },
+      ],
       responses: {
         "200": {
           description: "successful operation",
+          schema: {
+            type: "object",
+            properties: {
+              M: {
+                type: "array",
+                items: availabilityDef,
+              },
+              T: {
+                type: "array",
+                items: availabilityDef,
+              },
+              W: {
+                type: "array",
+                items: availabilityDef,
+              },
+              Th: {
+                type: "array",
+                items: availabilityDef,
+              },
+              F: {
+                type: "array",
+                items: availabilityDef,
+              },
+            },
+          },
         },
       },
     },
